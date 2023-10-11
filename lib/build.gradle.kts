c4499ce163b876513786ffa9a269a0bcdc3656ca
Reid Buzby <reid@ponto.org>
2023-10-11 12:06:05 -0700

    Rollback to kotlin 1.6

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
     id("maven-publish")
     id("com.adarshr.test-logger") version "3.2.0"
     id("org.jetbrains.dokka") version "1.9.0"
@@ -17,18 +17,18 @@ java {
 }
 
 dependencies {
-    implementation("io.grpc:grpc-kotlin-stub:1.4.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
+    implementation("io.grpc:grpc-kotlin-stub:1.3.1")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
     implementation("com.google.protobuf:protobuf-java-util:3.24.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
     testImplementation(kotlin("test"))
-    testImplementation("io.mockk:mockk:1.13.8")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("io.mockk:mockk:1.12.7")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("io.grpc:grpc-testing:1.58.0")
     testImplementation("io.grpc:grpc-examples:0.15.0")
     testImplementation("com.squareup.okhttp3:okhttp:4.10.0")
-    testImplementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    testImplementation("com.squareup.moshi:moshi-kotlin:1.13.0")
 }
 
 tasks.test {
